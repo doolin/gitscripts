c9be2e66570b87a644ca90cc54ffd1af58b9edde
Dave Doolin <david.doolin@gmail.com>
2011-09-01 10:31:10 -0700

    Added more commands to script

--- gitmaker.sh
+++ gitmaker.sh
@@ -18,4 +18,11 @@ echo "\nFile 1 second edit." >> file1.txt
 git add file1.txt
 git commit -m"Second commit for file1.txt"
 
+echo "Here is a second file" >> file2.txt
+git add file2.txt
+git commit -m"First commit for file2.txt"
+
+set foo = `git reflog`
+echo foo
+
 echo "Now do git show-branch --color --more=10"
