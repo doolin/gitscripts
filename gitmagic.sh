46c7360f88d105165cd1f1f7888eadd58e4fec87
Dave Doolin <david.doolin@gmail.com>
2011-09-12 07:25:57 -0700

    Start of Git Magic script.

---
+++ gitmagic.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "This script needs to be run from /tmp"
+#exit
+
+# Return values from command line:
+# http://git.661346.n2.nabble.com/command-return-values-td5279263.html
+
+rm -rf gittester
+
+mkdir "gittester"
+cd gittester
+git init
+
