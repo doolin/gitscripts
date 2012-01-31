5fd355a5d6ff7ce9385478185102839b431c6d64
Dave Doolin <david.doolin@gmail.com>
2012-01-31 09:30:23 -0800

    stubbed a rebase file.

---
+++ gitrebase.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+
+# http://randyfay.com/node/91
+
+# Everything below this line needs changing.
+
+echo "This script needs to be run from /tmp"
+#exit
+
+# Return values from command line:
+# http://git.661346.n2.nabble.com/command-return-values-td5279263.html
+
+rm -rf gittester
+
+mkdir "gittester"
+cd gittester
+git init
+
