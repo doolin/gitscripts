fc97cabbd0b8a86fd2582012f1fa932281cc9f5f
Dave Doolin <david.doolin@gmail.com>
2012-02-04 09:06:49 -0800

    started rebase example

--- gitrebase.sh
+++ gitrebase.sh
@@ -2,7 +2,10 @@
 
 
 # http://randyfay.com/node/91
+# http://book.git-scm.com/4_rebasing.html
 # http://darwinweb.net/articles/the-case-for-git-rebase
+# http://blog.woobling.org/2009/05/git-rebase-considered-awesome.html
+
 # Everything below this line needs changing.
 
 echo "This script needs to be run from /tmp"
